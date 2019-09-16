1e5b99dcd3b1c0a566712f6cff10ffe7c3719067
Luciano Xavier <luciano.xavier.lbv@gmail.com>
2019-09-16 19:33:09 -0300

    Projeto akazis 1.0

---
+++ index.php
@@ -0,0 +1,335 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1, user-scalable=no">
+        <title>AKAZIS</title>
+        <meta name="description" content=""/>
+        <meta name="robots" content="index, follow"/>
+
+        <link rel="canonical" href="">
+
+        <meta property="og:type" content="article"/>
+        <meta property="og:title" content="AKAZIS"/>
+        <meta property="og:description" content=""/>
+        <meta property="og:image" content=""/>
+        <meta property="og:url" content=""/>
+        <meta property="og:site_name" content="AKAZIS"/>
+        <meta property="og:locale" content="pt-br"/>
+
+        <meta itemprop="name" content="AKAZIS"/>
+        <meta itemprop="description" content=""/>
+        <meta itemprop="image" content=""/>
+        <meta itemprop="url" content=""/>
+
+        <link href="https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="style.css"/>
+        <link rel="stylesheet" href="_cdn/css/materialize.css"/>
+        <link rel="stylesheet" href="_cdn/css/unslider.css"/>
+        <link rel="stylesheet" href="_cdn/css/unslider-dots.css"/>
+
+
+    </head>
+    <body>
+        <header class="main_header container">
+            <div class="content row">
+                <div class="main_logo">
+                    <img src="img/logo-akazis.png" class="responsive-img">
+                </div>
+
+                <div class="main_menu ">
+                    <ul class="main_menu_box">
+                        <div class="main_menu_mobile_header">
+                            <i class="fas fa-times"></i>
+                            <p>Menu</p>
+                        </div>
+                        <li><a href="">Home</a></li>
+                        <li><a href="">Quem somos</a></li>
+                        <li><a href="">Materiais</a></li>
+                        <li><a href="">Blog</a></li>
+                        <li><a href="">Contato</a></li>
+                    </ul>
+
+                    <div class="menu_mobile">
+                        <i class="fas fa-bars"></i>
+                    </div>
+                </div>
+            </div>
+        </header>
+
+        <main>
+            <div class="main_hero container">
+                <div class="banner">
+                    <ul>
+                        <li><img src="img/slider-bg-1.jpg" class="responsive-img"></li>
+                        <li><img src="img/slider-bg-5.jpg" class="responsive-img"></li>
+                        <li><img src="img/slider-bg-1.jpg" class="responsive-img"></li>
+                    </ul>
+                </div>
+            </div>
+
+            <section class="main_new_products container">
+                <div class="content">
+                    <h1>Novos produtos</h1>
+                    <ul>
+                        <li class="new_product_item">
+                            <div class="new_product_img">
+                                <img src="img/13.jpg" class="responsive-img"
+                            </div>
+                            <div class="new_product_desc">
+                                <span>Cotton White Underweaer Block Out Edition</span>
+                            </div>
+                            <div class="new_product_price">
+                                <span>R$ 179,90</span>
+                            </div>
+                            <div class="new_product_button">
+                                <a href="#">Adicionar ao carrinho</a>
+                            </div>
+                        </li>
+
+                        <li class="new_product_item">
+                            <div class="new_product_img">
+                                <img src="img/13.jpg" class="responsive-img"
+                            </div>
+                            <div class="new_product_desc">
+                                <span>Cotton White Underweaer Block Out Edition</span>
+                            </div>
+                            <div class="new_product_price">
+                                <span>R$ 179,90</span>
+                            </div>
+                            <div class="new_product_button">
+                                <a href="#">Adicionar ao carrinho</a>
+                            </div>
+                        </li>
+
+                        <li class="new_product_item">
+                            <div class="new_product_img">
+                                <img src="img/13.jpg" class="responsive-img"
+                            </div>
+                            <div class="new_product_desc">
+                                <span>Cotton White Underweaer Block Out Edition</span>
+                            </div>
+                            <div class="new_product_price">
+                                <span>R$ 179,90</span>
+                            </div>
+                            <div class="new_product_button">
+                                <a href="#">Adicionar ao carrinho</a>
+                            </div>
+                        </li>
+
+                        <li class="new_product_item">
+                            <div class="new_product_img">
+                                <img src="img/13.jpg" class="responsive-img"
+                            </div>
+                            <div class="new_product_desc">
+                                <span>Cotton White Underweaer Block Out Edition</span>
+                            </div>
+                            <div class="new_product_price">
+                                <span>R$ 179,90</span>
+                            </div>
+                            <div class="new_product_button">
+                                <a href="#">Adicionar ao carrinho</a>
+                            </div>
+                        </li>
+
+                        <li class="new_product_item">
+                            <div class="new_product_img">
+                                <img src="img/13.jpg" class="responsive-img"
+                            </div>
+                            <div class="new_product_desc">
+                                <span>Cotton White Underweaer Block Out Edition</span>
+                            </div>
+                            <div class="new_product_price">
+                                <span>R$ 179,90</span>
+                            </div>
+                            <div class="new_product_button">
+                                <a href="#">Adicionar ao carrinho</a>
+                            </div>
+                        </li>
+
+                        <li class="new_product_item">
+                            <div class="new_product_img">
+                                <img src="img/13.jpg" class="responsive-img"
+                            </div>
+                            <div class="new_product_desc">
+                                <span>Cotton White Underweaer Block Out Edition</span>
+                            </div>
+                            <div class="new_product_price">
+                                <span>R$ 179,90</span>
+                            </div>
+                            <div class="new_product_button">
+                                <a href="#">Adicionar ao carrinho</a>
+                            </div>
+                        </li>
+                        
+                        <li class="new_product_item">
+                            <div class="new_product_img">
+                                <img src="img/13.jpg" class="responsive-img"
+                            </div>
+                            <div class="new_product_desc">
+                                <span>Cotton White Underweaer Block Out Edition</span>
+                            </div>
+                            <div class="new_product_price">
+                                <span>R$ 179,90</span>
+                            </div>
+                            <div class="new_product_button">
+                                <a href="#">Adicionar ao carrinho</a>
+                            </div>
+                        </li>
+                        
+                        <li class="new_product_item">
+                            <div class="new_product_img">
+                                <img src="img/13.jpg" class="responsive-img"
+                            </div>
+                            <div class="new_product_desc">
+                                <span>Cotton White Underweaer Block Out Edition</span>
+                            </div>
+                            <div class="new_product_price">
+                                <span>R$ 179,90</span>
+                            </div>
+                            <div class="new_product_button">
+                                <a href="#">Adicionar ao carrinho</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </section>
+
+            <div class="main_product_cta container">
+                <div class="content">
+                    <h1>Nova coleção de primavera</h1>
+                    <div class="main_product_cta_button">
+                        <a href="#">Vai perder?</a>
+                    </div>
+                </div>
+            </div>
+
+            <section class="main_blog container">
+                <div class="content row">
+                    <h1>Últimas notícias</h1>
+
+                    <div class="main_blog_box">
+                        <article>
+                            <a href="#">
+                                <img src="img/new-lastest-2-370x280.jpg" class="responsive-img">
+                                <h2>Tendências do verão 2020</h2>
+                                <span>Veja o que você deve se preparar para usar no próximo verão...</span>
+                                <div class="main_blog_article_button">Leia mais</div>
+                            </a>
+                        </article>
+
+                        <article>
+                            <a href="#">
+                                <img src="img/new-lastest-2-370x280.jpg" class="responsive-img">
+                                <h2>Tendências do verão 2020</h2>
+                                <span>Veja o que você deve se preparar para usar no próximo verão...</span>
+                                <div class="main_blog_article_button">Leia mais</div>
+                            </a>
+                        </article>
+
+                        <article>
+                            <a href="#">
+                                <img src="img/new-lastest-2-370x280.jpg" class="responsive-img">
+                                <h2>Tendências do verão 2020</h2>
+                                <span>Veja o que você deve se preparar para usar no próximo verão...</span>
+                                <div class="main_blog_article_button">Leia mais</div>
+                            </a>
+                        </article>
+
+                        <article>
+                            <a href="#">
+                                <img src="img/new-lastest-2-370x280.jpg" class="responsive-img">
+                                <h2>Tendências do verão 2020</h2>
+                                <span>Veja o que você deve se preparar para usar no próximo verão...</span>
+                                <div class="main_blog_article_button">Leia mais</div>
+                            </a>
+                        </article>
+
+                        <article>
+                            <a href="#">
+                                <img src="img/new-lastest-2-370x280.jpg" class="responsive-img">
+                                <h2>Tendências do verão 2020</h2>
+                                <span>Veja o que você deve se preparar para usar no próximo verão...</span>
+                                <div class="main_blog_article_button">Leia mais</div>
+                            </a>
+                        </article>
+
+                        <article>
+                            <a href="#">
+                                <img src="img/new-lastest-2-370x280.jpg" class="responsive-img">
+                                <h2>Tendências do verão 2020</h2>
+                                <span>Veja o que você deve se preparar para usar no próximo verão...</span>
+                                <div class="main_blog_article_button">Leia mais</div>
+                            </a>
+                        </article>
+                    </div>
+                </div>
+            </section>
+
+            <!--            <div class="main_newsletter container">
+                            <div class="content row">
+                                <div class="main_newsletter_title col l4 m4 s12">
+                                    <h1>Receba nossas promoções</h1>
+                                </div>
+            
+                                <div class="col l8 m8 s12">
+                                    <form action="#" method="post" accept-charset="utf-8" id="subscribe-form">
+                                        <div class="subscribe-content">
+                                            <div class="input">
+                                                <input type="email" name="subscribe-email" placeholder="Your Email">
+                                            </div>
+                                            <div class="button">
+                                                <button type="button">SUBCRIBE</button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>-->
+        </main>
+
+        <footer class="main_footer container">
+            <div class="content">
+                <div class="main_fotter_box">
+                    <ul>
+                        <li><a href="#">Sobre a Akazis</a></li>
+                        <li><a href="#">Blog</a></li>
+                        <li><a href="#">Contato</a></li>
+                        <li><a href="#">Loja</a></li>
+                    </ul>
+                </div>
+
+                <div class="main_fotter_box">
+                    <ul>
+                        <li><a href="#">Login/Registro</a></li>
+                        <li><a href="#">Seu carrinho</a></li>
+                        <li><a href="#">Coleções</a></li>
+                        <li><a href="#">Seu checkout</a></li>
+                    </ul>
+                </div>
+
+                <div class="main_fotter_box">
+                    <ul>
+                        <li><a href="#">FAQ's</a></li>
+                        <li><a href="#">Termos de serviços</a></li>
+                        <li><a href="#">Políticas de privacidade</a></li>
+                        <li><a href="#">Devoluções</a></li>
+                    </ul>
+                </div>
+                <div class="main_fotter_box">
+                    <img src="img/logo-akazis.png" class="responsive-img">
+                    <ul>
+                        <li>Rua Augusto Calheiros, Centenário - Caruaru/PE</li>
+                        <li>(81) 99926-9111</li>
+                        <li>atendimento@akazis.com.br</li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+
+        <script src="_cdn/js/jquery-3.3.1.min.js" type="text/javascript"></script>
+        <script src="_cdn/js/main.js" type="text/javascript"></script>
+        <script src="_cdn/js/materialize.js" type="text/javascript"></script>
+        <script src="_cdn/js/unslider.js" type="text/javascript"></script>
+    </body>
+</html>
